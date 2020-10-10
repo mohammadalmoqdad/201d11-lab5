b5c0c92eb478a7b0c2e9220e1bdf632a5a03823d
mohammadalmoqdad <almekad.mohammad@gmail.com>
2020-10-11 01:12:32 +0300

    try to add branch for p4

--- app.js
+++ app.js
@@ -72,10 +72,12 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
+
 function sumArray(sumArr) { //eslint-disable-line
     var sum1=sum(sum(sumArr[0], sumArr[1])[0],sumArr[2])[0];
     return [sum1,"2,3,4 was passed in as an array of numbers, and 9 is their sum." ];
 }
+//trying to add branch for p4
 testSumArray();
 // Here is the test for sumArray(); uncomment it to run it
 //testArray
